0b1f81c0448408a30c6d4514a3634df90a4c3740
Arief Aprilianto <48670284+ariefaprilianto@users.noreply.github.com>
2021-01-25 13:46:07 +0700

    Update failed bash script

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -14,41 +14,41 @@ declare -A failedtest
 # loop againts services
 for SERVICE in $SERVICES
 do
-  matrixtocheck["$SERVICE"]=0
+	matrixtocheck["$SERVICE"]=0
 done
 
 needrepocheck=0
 # loop againts files
 for FILE in $FILES
 do
-  case $FILE in
-    (*.go)  # ! == does match
-      servicecheckpoint=0
-      for SERVICE in $SERVICES
-      do
-        case $FILE  in
-          *"$SERVICE"*)
-            matrixtocheck["$SERVICE"]=1
-             ((servicecheckpoint=servicecheckpoint+1))
-             ;;
-          *)
-            matrixtocheck["$SERVICE"]=0 ;;
-        esac
-      done
-      if [ $servicecheckpoint -ne 0 ]; then
-        ((needrepocheck=needrepocheck+1))
-      fi
-  esac
+	case $FILE in
+	(*.go)  # ! == does match
+	    servicecheckpoint=0
+	    for SERVICE in $SERVICES
+	    do
+	        case $FILE  in
+	        *"$SERVICE"*)
+	            matrixtocheck["$SERVICE"]=1
+                ((servicecheckpoint=servicecheckpoint+1))
+	        ;;
+	        *)
+	            matrixtocheck["$SERVICE"]=0 ;;
+	        esac
+	    done
+	    if [ $servicecheckpoint -ne 0 ]; then
+            ((needrepocheck=needrepocheck+1))
+	    fi
+	esac
 done
 
 if [ $needrepocheck -eq 0 ]; then
-    echo "Changges applied to outside service, starting test..."
+	echo "Changges applied to outside service, starting test..."
     # repo tests
-    repowide="./..."
-    go test -v $(echo "$repowide") | tee test-repowide.out
-    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+	repowide="./..."
+	go test -v $(echo "$repowide") | tee test-repowide.out
+	if [ ${PIPESTATUS[0]} -ne 0 ]; then
         ((haserror=haserror+1))
-        failedtest[${#failedtest[@]}]="repowide"
+	    failedtest[${#failedtest[@]}]="repowide"
     fi
 else
     for SERVICE in "${!matrixtocheck[@]}"
@@ -57,43 +57,41 @@ else
         for val in "${matrixtocheck[$SERVICE]}"; do
             case $val in
             1)
-              echo "Changges applied to [$SERVICE] service, starting test..."
-              # database tests
-              db="./infrastructure/database/$SERVICE/..."
-              go test -v $(echo "$db") | tee test-db.out
-              if [ ${PIPESTATUS[0]} -ne 0 ]; then
-                ((haserror=haserror+1))
-                failedtest[${#failedtest[@]}]="database"
-              fi
-              # usecase tests
-              usecase="./usecase/$SERVICE/..."
-              go test -v $(echo "$usecase") | tee test-usecase.out
-              if [ ${PIPESTATUS[0]} -ne 0 ]; then
-                ((haserror=haserror+1))
-                failedtest[${#failedtest[@]}]="usecase"
-              fi
-              # web infra tests
-              web="./infrastructure/service/internal_/web/api/$SERVICE/..."
-              go test -v $(echo "$web") | tee test-web.out
-              if [ ${PIPESTATUS[0]} -ne 0 ]; then
-                ((haserror=haserror+1))
-                 failedtest[${#failedtest[@]}]="web"
-              fi
-
-
-                ;;
+                echo "Changges applied to [$SERVICE] service, starting test..."
+                # database tests
+                db="./infrastructure/database/$SERVICE/..."
+                go test -v $(echo "$db") | tee test-db.out
+                if [ ${PIPESTATUS[0]} -ne 0 ]; then
+                    ((haserror=haserror+1))
+                    failedtest[${#failedtest[@]}]="database"
+                fi
+                # usecase tests
+                usecase="./usecase/$SERVICE/..."
+                go test -v $(echo "$usecase") | tee test-usecase.out
+                if [ ${PIPESTATUS[0]} -ne 0 ]; then
+                    ((haserror=haserror+1))
+                    failedtest[${#failedtest[@]}]="usecase"
+                fi
+                # web infra tests
+                web="./infrastructure/service/internal_/web/api/$SERVICE/..."
+                go test -v $(echo "$web") | tee test-web.out
+                if [ ${PIPESTATUS[0]} -ne 0 ]; then
+                    ((haserror=haserror+1))
+                    failedtest[${#failedtest[@]}]="web"
+                fi
+            ;;
             esac
         done
     done
 fi
 
 if [ $haserror -ne 0 ]; then
-  echo "Warning! Some of unit tests failed. Test results are written in test-db.out, test-usecase.out, test-web.out, or test-repowide.out. Please check!"
-  for value in "${failedtest[@]}"
-  do
-      echo "hint: $value"
-  done
-  exit 1
+    echo "Warning! Some of unit tests failed. Test results are written in test-db.out, test-usecase.out, test-web.out, or test-repowide.out. Please check!"
+    for value in "${failedtest[@]}"
+    do
+        echo "hint: $value"
+    done
+    exit 1
 fi
 
 echo "All unit tests passed, continue pushing to repository..."
